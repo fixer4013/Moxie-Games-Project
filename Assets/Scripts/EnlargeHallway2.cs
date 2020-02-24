b4076be1b0498a240a0d240973ba1795fa59589b
kleine pomp <57906185+aarspiraat@users.noreply.github.com>
2020-02-24 13:56:39 +0100

    Full Project
    yes

---
+++ Assets/Scripts/EnlargeHallway2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnlargeHallway2 : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.position.z <= -36f)
+        {
+            transform.localScale = new Vector3(1, 1, 1 + (-player.position.z - 36)/6);
+            transform.position = new Vector3(50, 0, -72 - (-player.position.z - 36) * 4);
+        }
+        if (player.position.z > -36f)
+        {
+            transform.localScale = Vector3.one;
+            transform.position = new Vector3(50, 0, -72);
+        }
+    }
+}
---
+++ Assets/Scripts/testing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testing : MonoBehaviour
+{
+    public Vector3 dimension1;
+    public Vector3 dimension2;
+    int currentDimension;
+    Vector3 currenDimensionOffset;
+    public Transform player;
+
+    void Start()
+    {
+        currentDimension = 1;
+    }
+    void Update()
+    {
+        switch (currentDimension)
+        {
+            case 1:
+                currenDimensionOffset = dimension1;
+                break;
+            case 2:
+                currenDimensionOffset = dimension2;
+                break;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1) && currentDimension != 1)
+        {
+            player.position = player.position - currenDimensionOffset + dimension1;
+            currentDimension = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && currentDimension != 2)
+        {
+            player.position = player.position - currenDimensionOffset + dimension2;
+            currentDimension = 2;
+        }
+    }
+}
