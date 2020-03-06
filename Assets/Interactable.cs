1c697bcee12e6eb110fd955687373ad2d8925869
kleine pomp <57906185+aarspiraat@users.noreply.github.com>
2020-03-06 14:57:28 +0100

    New concept tryouts
    yes4

---
+++ Assets/Interactable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interactable : MonoBehaviour
+{
+    public GameObject door;
+    public Text text;
+
+    private void Update()
+    {
+        if (LookAtInteractable.currentObject == door)
+        {
+            text.enabled = true;
+        }
+        else
+        {
+            text.enabled = false;
+        }
+    }
+}
---
+++ Assets/LookAtInteractable.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtInteractable : MonoBehaviour
+{
+    float maxDistance = 2;
+
+    public static GameObject currentObject;
+
+    public Camera cam;
+
+    public LayerMask layerInteractables;
+
+    void FixedUpdate()
+    {
+
+        // Will contain the information of which object the raycast hit
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // if raycast hits, it checks if it hit an object with the tag Player
+        if (Physics.Raycast(ray, out hit, maxDistance, layerInteractables))
+
+        {
+            currentObject = hit.transform.gameObject;
+        }
+        else
+        {
+            currentObject = null;
+        }
+        Debug.Log(currentObject);
+    }
+}
