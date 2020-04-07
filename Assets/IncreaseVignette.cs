e5d25de5e254f807b3a2409bbc79c54157915d3c
kleine pomp <57906185+aarspiraat@users.noreply.github.com>
2020-04-07 12:24:33 +0200

    enemy doesnt chase immediately
    yes44

---
+++ Assets/IncreaseVignette.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class IncreaseVignette : MonoBehaviour
+{
+    public PostProcessVolume volume;
+    public Vignette vignette;
+    bool increase;
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume.profile.TryGetSettings(out vignette);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            increase = true;
+        }
+        if (increase)
+        {
+            vignette.intensity.value += Time.deltaTime * 0.2f;
+        }
+    }
+}
