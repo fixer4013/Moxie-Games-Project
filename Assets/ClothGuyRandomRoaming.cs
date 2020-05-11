93ea94f1d1630cc5ff61b7735b1437c2ca475d29
kleine pomp <57906185+aarspiraat@users.noreply.github.com>
2020-05-11 16:03:44 +0200

    Newer objects, update to smoke, bit of clean up
    yes52

---
+++ Assets/ClothGuyRandomRoaming.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClothGuyRandomRoaming : MonoBehaviour
+{
+    public Transform[] targets;
+    public NavMeshAgent nav;
+
+    public ParticleSystem par;
+    ParticleSystem.EmissionModule em;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        em = par.emission;
+        //StartCoroutine(RandomBurst());
+        em.rateOverTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!nav.hasPath)
+        {
+            ChooseTransform();
+        }
+    }
+
+    void ChooseTransform()
+    {
+        int rand = Random.Range(0, targets.Length);
+        nav.SetDestination(targets[rand].transform.position);
+    }
+
+    IEnumerator RandomBurst()
+    {
+        while (true)
+        {
+            var randomNum = Random.Range(10, 15);
+            em.rateOverTime = 0;
+            yield return new WaitForSeconds(randomNum);
+            em.rateOverTime = 100;
+            var currentTime = Time.time;
+            Debug.Log(currentTime);
+            while (Time.time < currentTime + 1)
+            {
+                nav.velocity = nav.transform.forward * 10;
+                yield return 0;
+            }
+        }
+
+    }
+}
--- Assets/EnemySmokeTest.cs
+++ Assets/EnemySmokeTest.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class EnemySmokeTest : MonoBehaviour
 {
     public ParticleSystem par;
-    public ParticleSystem par2;
     ParticleSystem.EmissionModule em;
+    public ParticleSystem par2;
     ParticleSystem.EmissionModule em2;
     public float speed;
+
+    MeshRenderer mr;
+    public GameObject par3;
+
     // Start is called before the first frame update
     void Start()
     {
         em = par.emission;
         em2 = par2.emission;
+        mr = GetComponent<MeshRenderer>();
     }
 
     // Update is called once per frame
@@ -22,31 +27,32 @@ public class EnemySmokeTest : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
-            em.rateOverTime = 60;
-            em2.rateOverTime = 60;
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += new Vector3(0, 0, -speed) * Time.deltaTime;
-            em.rateOverTime = 60;
-            em2.rateOverTime = 60;
         }
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
-            em.rateOverTime = 60;
-            em2.rateOverTime = 60;
         }
         if (Input.GetKey(KeyCode.S))
         {
             transform.position += new Vector3(0, -speed, 0) * Time.deltaTime;
-            em.rateOverTime = 60;
-            em2.rateOverTime = 60;
         }
         if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
         {
             em.rateOverTime = 0;
             em2.rateOverTime = 0;
+            mr.enabled = true;
+            par3.SetActive(true);
+        }
+        else
+        {
+            em.rateOverTime = 100;
+            em2.rateOverTime = 40;
+            mr.enabled = false;
+            par3.SetActive(false);
         }
     }
 }
